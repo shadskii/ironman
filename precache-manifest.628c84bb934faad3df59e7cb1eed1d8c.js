92dfc74bc992617277adaf1fe25e1ceeeb7017cb
Jake Hassel <jake.k.hassel@gmail.com>
2018-11-16 14:14:08 -0500

    Updates

---
+++ precache-manifest.628c84bb934faad3df59e7cb1eed1d8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ironman/robots.txt"
+  },
+  {
+    "revision": "50acb5403dbb04d90bb3",
+    "url": "/ironman/js/chunk-vendors.628f6bf7.js"
+  },
+  {
+    "revision": "feb1c8e0d4d68c743f80",
+    "url": "/ironman/js/app.7d646109.js"
+  },
+  {
+    "revision": "27014df55f126546840d90feb0d7b9d5",
+    "url": "/ironman/index.html"
+  },
+  {
+    "revision": "468e19d7b6126557ab7c4c1667aa247c",
+    "url": "/ironman/img/ironman.468e19d7.png"
+  },
+  {
+    "revision": "50acb5403dbb04d90bb3",
+    "url": "/ironman/css/chunk-vendors.0a7191b6.css"
+  },
+  {
+    "revision": "feb1c8e0d4d68c743f80",
+    "url": "/ironman/css/app.01ca4fa2.css"
+  }
+];
